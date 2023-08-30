500d200f8ae5c1fb5fa13c4ab24154ed53354d4c
juliaquiterio <juliapquiterio@gmail.com>
2023-08-29 22:05:28 -0300

    update Teste.java

--- padroes/strategy/Curso/src/strategy/Teste.java
+++ padroes/strategy/Curso/src/strategy/Teste.java
@@ -1,5 +1,19 @@
 package strategy;
 
 public class Teste {
+	
+	public static void main(String[] args) {
+		
+		Aluno claudinho = new Aluno();
+		
+		claudinho.setCurso(new BancoDados());
+		
+		claudinho.fazerCurso();
+
+		claudinho.setCurso(new AnaliseDevSistemas());
+		
+		claudinho.fazerCurso();
+		
+	}
 
 }
